ac0f3ba3edfe98a894618324290f3ae67d078f8c
GeovaneF55 <geovanefss@gmail.com>
2019-12-13 10:43:30 -0300

    Aula 2

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,10 +1,27 @@
-
 const express = require('express')
-module.exports = function(server) {
-    // Definir URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
-    // Rotas de Ciclo de Pagamento
-    // const BillingCycle = require('../api/billingCycle/billingCycleService')
-    // BillingCycle.register(router, '/billingCycles')
+const auth = require('./auth')
+
+module.exports = function (server) {
+
+    /*
+     * Rotas protegidas por Token JWT
+     */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    /*
+     * Rotas abertas
+     */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
